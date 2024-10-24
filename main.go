a2e49e40f575bf15adbd3706867a7c26fdbd0731
Andrey Chiglintcew <melanj.ks.ua@gmail.com>
2024-10-24 23:38:19 +0300

    Step 4 (20l) controller one route

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
+	"github.com/Melanjnk/REST_API_GO/m/controllers"
 	"github.com/Melanjnk/REST_API_GO/m/drivers"
 	"github.com/Melanjnk/REST_API_GO/m/models"
 	"github.com/gorilla/mux"
@@ -11,7 +11,6 @@ import (
 	_ "github.com/subosito/gotenv"
 	"log"
 	"net/http"
-	"strconv"
 )
 
 type Book struct {
@@ -19,7 +18,6 @@ type Book struct {
 }
 
 var db *sql.DB
-var books []Book
 
 func init() {
 	gotenv.Load()
@@ -41,85 +39,15 @@ func main() {
 
 	router.StrictSlash(true)
 
-	router.HandleFunc("/books", getBooks).Methods("GET")
-	router.HandleFunc("/books/{id}", getBook).Methods("GET")
-	router.HandleFunc("/books", addBook).Methods("POST")
-	router.HandleFunc("/books/{id}", updateBook).Methods("PUT")
-	router.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
+	conrollers := controllers.Controller{}
+
+	router.HandleFunc("/books", conrollers.GetBooks(db)).Methods("GET")
+	//router.HandleFunc("/books/{id}", getBook).Methods("GET")
+	//router.HandleFunc("/books", addBook).Methods("POST")
+	//router.HandleFunc("/books/{id}", updateBook).Methods("PUT")
+	//router.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 
 	fmt.Println("Hello World")
-}
-
-func getBooks(w http.ResponseWriter, r *http.Request) {
-	books = make([]Book, 0)
-	book := &Book{}
-	rows, err := db.Query("select * from books")
-	drivers.LogFatal(err)
-
-	defer rows.Close()
-
-	for rows.Next() {
-		err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
-		drivers.LogFatal(err)
-		books = append(books, *book)
-	}
-	json.NewEncoder(w).Encode(books)
-}
-
-func getBook(w http.ResponseWriter, r *http.Request) {
-	book := &Book{}
-	params := mux.Vars(r)
-
-	id, _ := strconv.Atoi(params["id"])
-
-	rows := db.QueryRow("select * from books where id = $1", id)
-	err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
-	drivers.LogFatal(err)
-
-	json.NewEncoder(w).Encode(&book)
-}
-
-func (b *Book) String() string {
-	return fmt.Sprintf("ID: %d, Title: %s, Author: %s", b.ID, b.Title, b.Author)
-}
-
-func addBook(w http.ResponseWriter, r *http.Request) {
-	book := &Book{}
-	_ = json.NewDecoder(r.Body).Decode(&book)
-	log.Println(book)
-
-	row := db.QueryRow("insert into books (title, author, year) values ($1, $2, $3) RETURNING id;", book.Title, book.Author, book.Year)
-	err := row.Scan(&book.ID)
-	drivers.LogFatal(err)
-	json.NewEncoder(w).Encode(book)
-}
-
-func updateBook(w http.ResponseWriter, r *http.Request) {
-	book := &Book{}
-	_ = json.NewDecoder(r.Body).Decode(&book)
-	log.Println(book)
-
-	result, err := db.Exec("update books set title=$1, author=$2, year=$3 where id = $4 RETURNING id;",
-		book.Title, book.Author, book.Year, book.ID)
-	drivers.LogFatal(err)
-	rowsUpdated, err := result.RowsAffected()
-	drivers.LogFatal(err)
-	json.NewEncoder(w).Encode(rowsUpdated)
-}
-
-func removeBook(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-
-	id, _ := strconv.Atoi(params["id"])
-
-	result, err := db.Exec("delete from books where id = $1",
-		id)
-	drivers.LogFatal(err)
-
-	rowsDeleted, err := result.RowsAffected()
-	drivers.LogFatal(err)
-
-	json.NewEncoder(w).Encode(rowsDeleted)
 }
